80799a81c98d8c06e52ac00c23d5e8c2a214d555
saber71 <1533357473@qq.com>
2024-05-09 16:05:36 +0800

    Convert number to chinese (#14)
    * feat: Implement chinese number conversion function
    
    Added a function to convert a number into Chinese characters.
    Included support for converting integers and decimal places.
    Implemented unit conversions up to 100 million.
    Introduced tests for the conversion function.
    
    * refactor: move chineseNumber to common

---
+++ src/packages/common/chineseNumber.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest"
+import { chineseNumber } from "./chineseNumber"
+
+test("chineseNumber", () => {
+  expect(chineseNumber(123)).toBe("一百二十三")
+  expect(chineseNumber(123.0123)).toBe("一百二十三点零一二三")
+  expect(chineseNumber(1234)).toBe("一千二百三十四")
+  expect(chineseNumber(1034)).toBe("一千零三十四")
+  expect(chineseNumber(12301234)).toBe("一千二百三十万一千二百三十四")
+  expect(chineseNumber(1034567890)).toBe("十亿三千四百五十六万七千八百九十")
+})
